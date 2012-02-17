b80b0b629f2ac2ee75077cf0eae26fd602ddc2bb
gquadrossilva <gquadrossilva@e532198c-ef0b-2729-7a85-03f52e7a54eb>
2012-02-17 10:03:18 +0000

    Removing static analysis

--- trunk/AddrLeaks/run.sh
+++
@@ -1,3 +0,0 @@
-opt -load ../../../Release/lib/AddrLeaks.so -instnamer -internalize -inline -addrleaks < "tests/$1.bc" > /dev/null &&
-dot -Tpng module.dot -o module.png &&
-eog module.png
--- trunk/AddrLeaks/tests/t1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-main() {
-    int a[5], b;
-    a[0] = &b;
-
-    printf("%d\n", a[3]);
-}
