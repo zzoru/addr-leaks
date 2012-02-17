2bd6cfb49924e7bd2bf0ad48f747fe2ce653adee
gquadrossilva <gquadrossilva@e532198c-ef0b-2729-7a85-03f52e7a54eb>
2012-02-17 09:46:14 +0000

    Added static analysis.

---
+++ trunk/AddrLeaks/PointerAnalysis.h
@@ -0,0 +1,76 @@
+#ifndef POINTER_ANALYSIS_H
+#define POINTER_ANALYSIS_H
+
+#include <set>
+#include <map>
+#include <deque>
+
+// ============================================= //
+
+typedef std::set<int> IntSet;
+typedef std::map<int, IntSet> IntSetMap;
+typedef std::map<int, int> IntMap;
+typedef std::deque<int> IntDeque;
+
+// ============================================= //
+
+class PointerAnalysis {
+    
+    public:
+        PointerAnalysis();
+        ~PointerAnalysis();
+
+        // Add a constraint of type: A = &B
+        void addAddr(int A, int B);
+
+        // Add a constraint of type: A = B
+        void addBase(int A, int B);
+
+        // Add a constraint of type: *A = B
+        void addStore(int A, int B);
+
+        // Add a constraint of type: A = *B
+        void addLoad(int A, int B);
+
+        // Execute the pointer analysis
+        void solve();
+
+        // Return the set of positions pointed by A:
+        //   pointsTo(A) = {B1, B2, ...}
+        std::set<int>  pointsTo(int A);
+	
+        // Return the points-to map
+        std::map<int, std::set<int> > allPointsTo();
+
+		void print();
+	
+	private:
+		void addNode(int id);
+		void addEdge(int fromId, int toId);
+		void addToPts(int pointed, int pointee);
+		void merge(int id, int target);
+		void visit(int Node, IntMap& Order, IntMap& Repr, int& idxOrder,
+			IntSet& Curr, IntDeque& Stack);
+        void removeCycles();
+	
+		// Hold the points-to Set
+		IntSetMap pointsToSet;
+
+		// Hold the vertices and their representatives
+        IntMap vertices;
+		
+		// Hold the active vertices
+		IntSet activeVertices;
+		
+		// Hold the graph structure
+        IntSetMap from;
+        IntSetMap to;
+
+		// Hold the complex constraints
+        IntSetMap loads;
+        IntSetMap stores;
+};
+
+// ============================================= //
+
+#endif  /* POINTER_ANALYSIS_H */
