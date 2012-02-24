55c3fe93474fcd551d6d92da9991728da876e052
gquadrossilva <gquadrossilva@e532198c-ef0b-2729-7a85-03f52e7a54eb>
2012-02-24 02:22:20 +0000

    Included Constants.h to avoid error messages during compilation on Mac.

--- AddrLeaks/AddrLeaks.cpp
+++ AddrLeaks/AddrLeaks.cpp
@@ -27,6 +27,7 @@
 #include "llvm/Instructions.h"
 #include "llvm/InstrTypes.h"
 #include "llvm/Pass.h"
+#include "llvm/Constants.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/CallSite.h"
 #include "llvm/ADT/Statistic.h"
