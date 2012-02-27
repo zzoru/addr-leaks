c3d49f65d107958c6e02887e2606c4fa0e9fa4a4
gquadrossilva <gquadrossilva@e532198c-ef0b-2729-7a85-03f52e7a54eb>
2012-02-27 15:28:58 +0000

    Added code to help debug pointer analysis.

--- trunk/AddrLeaks/run.sh
+++ trunk/AddrLeaks/run.sh
@@ -1,3 +1,3 @@
-opt -load ../../../Release/lib/AddrLeaks.so -instnamer -internalize -inline -addrleaks < "$1" > /dev/null &&
+opt -load ../../../Release/lib/AddrLeaks.so -instnamer -internalize -inline -addrleaks < "$1" #> /dev/null &&
 dot -Tpng module.dot -o module.png &&
 eog module.png
