fb8102eeb50ea0c52fa1f5e6db1d7b5ba3fc46a1
gquadrossilva <gquadrossilva@e532198c-ef0b-2729-7a85-03f52e7a54eb>
2012-03-05 02:30:14 +0000

    Fixed struct1 test checking.

--- AddrLeaks/tests/run.py
+++ AddrLeaks/tests/run.py
@@ -87,28 +87,23 @@ for filename in files:
         if result.find('File: array4.c, Line: 17') != -1: passed = False
         if result.find('File: array4.c, Line: 20') == -1: passed = False
     elif filename == 'struct1.bc':
-        if result.find('File: struct1.c, Line: 15') != -1: passed = False
+        if result.find('File: struct1.c, Line: 15') == -1: passed = False
         if result.find('File: struct1.c, Line: 16') != -1: passed = False
-        if result.find('File: struct1.c, Line: 17') != -1: passed = False
-        if result.find('File: struct1.c, Line: 23') != -1: passed = False
-        if result.find('File: struct1.c, Line: 24') != -1: passed = False
-        if result.find('File: struct1.c, Line: 25') == -1: passed = False
-        if result.find('File: struct1.c, Line: 31') == -1: passed = False
-        if result.find('File: struct1.c, Line: 32') != -1: passed = False
-        if result.find('File: struct1.c, Line: 33') == -1: passed = False
+        if result.find('File: struct1.c, Line: 17') == -1: passed = False
     elif filename == 'struct2.bc':
-        if result.find('File: struct2.c, Line: 20') != -1: passed = False
-        if result.find('File: struct2.c, Line: 21') != -1: passed = False
-        if result.find('File: struct2.c, Line: 22') != -1: passed = False
-        if result.find('File: struct2.c, Line: 23') != -1: passed = False
-        if result.find('File: struct2.c, Line: 30') != -1: passed = False
-        if result.find('File: struct2.c, Line: 31') != -1: passed = False
-        if result.find('File: struct2.c, Line: 32') == -1: passed = False
-        if result.find('File: struct2.c, Line: 33') != -1: passed = False
-        if result.find('File: struct2.c, Line: 40') == -1: passed = False
-        if result.find('File: struct2.c, Line: 41') != -1: passed = False
-        if result.find('File: struct2.c, Line: 42') == -1: passed = False
-        if result.find('File: struct2.c, Line: 43') == -1: passed = False
+        passed = False
+        #if result.find('File: struct2.c, Line: 20') != -1: passed = False
+        #if result.find('File: struct2.c, Line: 21') != -1: passed = False
+        #if result.find('File: struct2.c, Line: 22') != -1: passed = False
+        #if result.find('File: struct2.c, Line: 23') != -1: passed = False
+        #if result.find('File: struct2.c, Line: 30') != -1: passed = False
+        #if result.find('File: struct2.c, Line: 31') != -1: passed = False
+        #if result.find('File: struct2.c, Line: 32') == -1: passed = False
+        #if result.find('File: struct2.c, Line: 33') != -1: passed = False
+        #if result.find('File: struct2.c, Line: 40') == -1: passed = False
+        #if result.find('File: struct2.c, Line: 41') != -1: passed = False
+        #if result.find('File: struct2.c, Line: 42') == -1: passed = False
+        #if result.find('File: struct2.c, Line: 43') == -1: passed = False
     elif filename == 'context1.bc':
         passed = True
     elif filename == 'inter1.bc':
