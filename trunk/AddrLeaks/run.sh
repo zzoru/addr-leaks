1b2d3ec9bd568bd2c91a2072ce8af3a91a2655c8
gquadrossilva <gquadrossilva@e532198c-ef0b-2729-7a85-03f52e7a54eb>
2012-03-28 00:17:40 +0000

    Fixed run script.

--- trunk/AddrLeaks/run.sh
+++ trunk/AddrLeaks/run.sh
@@ -1,3 +1,3 @@
-opt -load ../../../Release/lib/AddrLeaks.so -mem2reg -instnamer -internalize -inline -globaldce -addrleaks < "$1" #> /dev/null &&
+opt -load ../../../Release/lib/AddrLeaks.so -mem2reg -instnamer -internalize -inline -globaldce -addrleaks < "$1" > /dev/null &&
 dot -Tpng module.dot -o module.png &&
 eog module.png
