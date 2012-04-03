923d9bf251792fc147a4b79fc0070efa5d26d63e
gquadrossilva <gquadrossilva@e532198c-ef0b-2729-7a85-03f52e7a54eb>
2012-04-03 02:49:54 +0000

    Added initial support for string analysis.

--- AddrLeaks/tests/strings1.c
+++ AddrLeaks/tests/strings1.c
@@ -9,10 +9,10 @@ int main() {
     int n = (int)&n;
     
     itoa(n, s, 10);
-    printf("%s\n", s);
+    printf("%s \n", s);
 
     sprintf(s, "test string %p\n", s);
-    printf("%s\n", s);
+    printf("%s \n", s);
 
     return 0;
 }
