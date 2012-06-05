98d5b7ebbb5b6fac3f2f239e653a7845658ffc45
redmajor@gmail.com <redmajor@gmail.com@e532198c-ef0b-2729-7a85-03f52e7a54eb>
2012-06-05 03:19:47 +0000

    Mods

--- Instrumentation/hash.c
+++ Instrumentation/hash.c
@@ -1,17 +1,17 @@
 
 #include <sys/mman.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+unsigned long MAX_MEMORY = 1UL << 47;
 
-#define MEM_BEGIN  0x400000
-#define MEM_END    0x800000000000
-#define MEM_MID    0x400000000000
-#define BLOCK_SIZE  0x10000000000
+void* begin;
 
 void createShadowMemory()
 {
-	void* result = mmap(MEM_MID - BLOCK_SIZE, 3 * BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, 0, 0); 
-	if (result == MAP_FAILED) {
+	void* begin = mmap(0, MAX_MEMORY / 2, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, 0, 0); 
+	if (begin== MAP_FAILED) {
 		printf("Instrumentation MMAP failed. errno=%d\n", errno);
 		exit(-1);
 	}
@@ -19,16 +19,13 @@ void createShadowMemory()
 
 void* translate(void* addr)
 {
-	if (addr >=  0x7f0000000000)
+	if (addr <= begin)
 	{
-
-		long offset = MEM_END -  (long) addr;
-		return MEM_MID - offset;
+		 return begin + (long) addr;
 	}
 	else if (addr >= 0x010000000000)
 	{
-        long offset = addr - ((long)addr & 0xff0000000000);
-        return MEM_MID + BLOCK_SIZE + offset;
+		return begin - MAX_MEMORY / 2 + (long) addr;
 	}
     else // 0x00XXXXXXXXXX
     {
@@ -42,45 +39,3 @@ void myAbort()
 	printf("An address was leaked\n");
 	exit(-1);
 }
-
-void assertString(char* str)
-{
-	char* shadowStr = translate(str);
-	
-	while (*str != 0)
-	{
-		if (*shadowStr)
-		{
-			myAbort();
-		}
-		
-		str++;
-		shadowStr++;
-	}
-}
-
-
-//void* createArgvShadow(int argc, void** argv) //@todo Need to discover how to make it i32 rather than int (there are other solutions of course)
-//{
-//	for (int i = 0; i < argc; i++)
-//	{
-//		void* p = argv[i];
-//		
-//		while (*(int*)p != 0)
-//		{
-//			p++;
-//		}
-//		
-//		int size = p - argv[i];
-//		void* shadow = malloc(size * 8);
-//		addInHash(argv[i], shadow);
-//		memset(shadow, 0, size * 8);
-//	}
-//	
-//	char** argvShadow = malloc(sizeof(void*) * argc);
-//	
-//	addInHash(argv, argvShadow);
-//	memset(argvShadow, -1, argc * sizeof(void*));
-//	return argvShadow;
-//}
-
